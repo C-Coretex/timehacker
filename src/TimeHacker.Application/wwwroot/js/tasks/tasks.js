8cafb7ecd8ce46788ca917e6e4501bdda4924980
V Sh <->
2024-02-19 20:11:52 +0200

    tasks.js - manage buttons init

--- src/TimeHacker.Application/wwwroot/js/tasks/tasks.js
+++ src/TimeHacker.Application/wwwroot/js/tasks/tasks.js
@@ -1,15 +1,45 @@
-$(".js-open-fixed-tasks-button").on("click", () => {
-    $("#openFixedTasksLi").addClass("active");
-    $("#openDynamicTasksLi").removeClass("active");
+$('.js-open-fixed-tasks-button').on('click', () => {
+    $('#openFixedTasksLi').addClass('active')
+    $('#openDynamicTasksLi').removeClass('active')
 
-    $(".js-fixed-tasks").removeClass("d-none");
-    $(".js-dynamic-tasks").addClass("d-none");
-});
+    $('.js-fixed-tasks').removeClass('d-none')
+    $('.js-dynamic-tasks').addClass('d-none')
+})
 
-$(".js-open-dynamic-tasks-button").on("click", () => {
-    $("#openDynamicTasksLi").addClass("active");
-    $("#openFixedTasksLi").removeClass("active");
+$('.js-open-dynamic-tasks-button').on('click', () => {
+    $('#openDynamicTasksLi').addClass('active')
+    $('#openFixedTasksLi').removeClass('active')
 
-    $(".js-dynamic-tasks").removeClass("d-none");
-    $(".js-fixed-tasks").addClass("d-none");
-});
+    $('.js-dynamic-tasks').removeClass('d-none')
+    $('.js-fixed-tasks').addClass('d-none')
+})
+
+
+$('.js-button-edit-fixed-task').on('click', function () {
+    const self = this
+    const taskContainer = $(this).closest('.task-container')
+    const taskId = $(taskContainer).attr('item-id')
+    console.log(taskId)
+})
+
+$('.js-button-delete-fixed-task').on('click', function () {
+    const self = this
+    const taskContainer = $(this).closest('.task-container')
+    const taskId = $(taskContainer).attr('item-id')
+    console.log(taskId)
+})
+
+
+$('.js-button-edit-dynamic-task').on('click', function () {
+    const self = this
+    const taskContainer = $(this).closest('.task-container')
+    const taskId = $(taskContainer).attr('item-id')
+    console.log(taskId)
+})
+
+$('.js-button-delete-dynamic-task').on('click', function () {
+    const self = this
+    const taskContainer = $(this).closest('.task-container')
+    const taskId = $(taskContainer).attr('item-id')
+    console.log(taskId)
+})
