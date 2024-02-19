a334880fef032a396f8707419027702f5b553936
V Sh <->
2024-02-19 18:50:51 +0200

    performance fix

--- src/Helpers.Domain/Helpers/RandomValuesHelper.cs
+++ src/Helpers.Domain/Helpers/RandomValuesHelper.cs
@@ -8,20 +8,22 @@ namespace Helpers.Domain.Helpers
 {
     public static class RandomValuesHelper
     {
-        public static IEnumerable<TEntry> GetRandomEntries<TEntry>(IEnumerable<(TEntry Entry, float Weight)> entries, int count)
+        public static IEnumerable<TEntry> GetRandomEntries<TEntry>(IEnumerable<(TEntry Entry, float Weight)> entries, int count, Random? random = null)
         {
             ArgumentNullException.ThrowIfNull(entries);
             if (count < 0)
                 throw new ArgumentOutOfRangeException(nameof(count), "Count must be greater than or equal to 0");
 
+            random ??= Random.Shared;
+
             var entriesList = entries.OrderBy(e => e.Weight).ToList();
             if (count > entriesList.Count)
                 count = entriesList.Count;
 
+            var totalWeight = entriesList.Sum(e => e.Weight);
             for (int i = 0; i < count; i++)
             {
-                var totalWeight = entriesList.Sum(e => e.Weight);
-                var randomValue = Random.Shared.NextDouble() * totalWeight;
+                var randomValue = random.NextDouble() * totalWeight;
 
                 foreach (var entry in entriesList)
                 {
@@ -29,6 +31,8 @@ namespace Helpers.Domain.Helpers
                     if (randomValue <= 0)
                     {
                         yield return entry.Entry;
+
+                        totalWeight -= entry.Weight;
                         entriesList.Remove(entry);
                         break;
                     }
